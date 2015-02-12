e5d36e249d079e5d1e7694e3def04b47af6a10e9
quadnix <quadnix1@gmail.com>
2015-02-12 14:05:28 -0500

    edited main.java

--- src/org/kagelabs/pkbasic/Main.java
+++ src/org/kagelabs/pkbasic/Main.java
@@ -1,5 +1,6 @@
 package org.kagelabs.pkbasic;
 
+
 /**
  * Program entry point
  * @author Patrick Kage
@@ -8,5 +9,12 @@ package org.kagelabs.pkbasic;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("It's alive!");
+		Main main = new Main();
+		main.run();
+	}
+	
+	public void run() {
+		Interpreter pkb = new Interpreter();
+		pkb.initialize("pkb/fullexample.pkb");
 	}
 }
