a9c63e8d6c02544681db3d1fdb73276cb536a826
Zhongkai-Zheng <2277428997@qq.com>
2015-02-13 12:12:47 -0500

    added pullActions()

--- src/org/kagelabs/pkbasic/ActionProcessor.java
+++ src/org/kagelabs/pkbasic/ActionProcessor.java
@@ -1,10 +1,25 @@
 package org.kagelabs.pkbasic;
 
+/**
+ * Class to keep track of errors emitted by the project in various states of failure.
+ * @author Patrick Kage 
+ * @author Alex Zheng
+ *
+ */
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ActionProcessor {
 	private ArrayList<ActionProvider> actionProviders;
+	private HashMap<ActionMetadata, Action> actions;
+	
+	public ActionProcessor(){
+		this.actionProviders = new ArrayList<ActionProvider>();
+		this.actions = new HashMap<ActionMetadata, Action>();
+	}
 	
 	public boolean importLibrary(ErrorHandler eh, String refname) {
 		try {
@@ -17,17 +32,24 @@ public class ActionProcessor {
 			
 			// what the fuck am i doing
 			Class loadedClass = cl.loadClass(refname);
-			
 			Object loadedInstance = loadedClass.newInstance();
 			Method getActionProvider = loadedClass.getMethod("getActionProvider", new Class[]{});
 			ActionProvider ap = (ActionProvider) getActionProvider.invoke(loadedInstance);
 			actionProviders.add(ap);
-			
+	
 		} catch (Exception e) {
 			eh.addError(new Error("LibraryImport", "Failed to import " + refname, "ActionProcessor"));
 			return false;
 		}
 		return true;
-		
+	}
+
+	private HashMap<ActionMetadata, Action> pullActions() {
+		// TODO Auto-generated method stub
+		HashMap<ActionMetadata, Action> returnedHashMap = new HashMap<ActionMetadata, Action>();
+		for(int i=0; i<this.actionProviders.size(); i++){
+		returnedHashMap.putAll(actionProviders.get(i).getActionDictionary());
+		}
+		return returnedHashMap; 
 	}
 }
