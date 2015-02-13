c67ec608d79e85989e26c8fa220aacfea89a86b5
TotesRiteous <chastings18@choate.edu>
2015-02-13 11:58:32 -0500

    Testing Splitter

--- src/org/kagelabs/pkbasic/Main.java
+++ src/org/kagelabs/pkbasic/Main.java
@@ -8,13 +8,24 @@ package org.kagelabs.pkbasic;
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("It's alive!");
-		Main main = new Main();
-		main.run();
+		//System.out.println("It's alive!");
+		/*Main main = new Main();
+		main.run();*/
+		test();
 	}
 	
 	public void run() {
 		Interpreter pkb = new Interpreter();
 		pkb.initialize("pkb/fullexample.pkb");
+	}
+	
+	public static void test() {
+		Directive directive = new Directive("test \"test test test\" test");
+		System.out.println(directive.getFull());
+		for(int i = 0; i < directive.getSplit().size(); i++) {
+			System.out.println(directive.getSplit().get(i)+" "+i);
+		}
+		System.out.println(directive.getSplit().size());
+		
 	}
 }
