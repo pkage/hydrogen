73ec0e31c85dd8c0db1d1f11679edd8a03bf3f1e
Zhongkai-Zheng <2277428997@qq.com>
2015-02-13 11:22:04 -0500

    Error Handler & Error
    I finished the generate report method

--- src/org/kagelabs/pkbasic/ErrorHandler.java
+++ src/org/kagelabs/pkbasic/ErrorHandler.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 
 /**
  * Class to keep track of errors emitted by the project in various states of failure.
- * @author Patrick Kage
+ * @author Patrick Kage && Alex Zheng
  *
  */
 public class ErrorHandler {
@@ -20,7 +20,26 @@ public class ErrorHandler {
 	
 	public String generateReport() {
 		// do some shit here
-		return "Error reporting NYI";
+		String[] errorNames = new String[stack.size()];
+		String[] errorReasons = new String[stack.size()];
+		String[] errorOrigins = new String[stack.size()];
+		Object[] stack2 = stack.toArray();
+		for(int i = 0; i<stack.size(); i++){
+			errorNames[i] = ((Error) stack2[i]).getName();
+		}
+		for(int i = 0; i<stack.size(); i++){
+			errorReasons[i] = ((Error) stack2[i]).getReason();
+		}
+		for(int i = 0; i<stack.size(); i++){
+			errorOrigins[i] = ((Error) stack2[i]).getOrigin();
+		}
+		String errorReport = "ERROR: /n";
+		for(int i = 0; i<stack.size(); i++){
+			errorReport += "Error Type: " + errorNames[i] + "   ";
+			errorReport += "Error Reason: " + errorReasons[i] + "   ";
+			errorReport += "Error Origin: " + errorOrigins[i] + "    /n"; 
+		}
+		return errorReport;
 	}
 	
 	public boolean hasErrors() {
