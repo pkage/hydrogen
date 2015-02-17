ae8edd9698afcaa29ed8589848607e4f6304b213
quadnix <quadnix1@gmail.com>
2015-02-17 14:10:44 -0500

    updated actionprocessor to load out of jars

--- src/org/kagelabs/hydrogen/ActionProcessor.java
+++ src/org/kagelabs/hydrogen/ActionProcessor.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.*;
 
 public class ActionProcessor {
 	private ArrayList<ActionProvider> actionProviders;
@@ -26,21 +27,35 @@ public class ActionProcessor {
 		try {
 			
 			// get the location of the aux binaries
-			URL location[] = {new URL("file:///home/patrick/workspace/pkbasic/bin/org/kagelabs/hydrogen/stl/"),
-							  new URL("file:///usr/local/hydrogen/lib/"),
+			URL location[] = {
+							  new URL("file:///usr/local/hydrogen/lib/io2.jar"),
 							  new URL("http://packages.kagelabs.org/host/")
 							 };
 			
 			// create a class loader with the location
 			URLClassLoader loader = new URLClassLoader(location);
 			
+			
+			
 			// extract the class
 			Class<?> lib = loader.loadClass(name);
-			System.out.println("loaded library");
+			System.out.println("loaded main class");
 			
+			
+			// load inner classes
+			Method getAPcname = lib.getDeclaredMethod("getProviderName");
+			String apcname = (String)getAPcname.invoke(lib.newInstance());
+			loader.loadClass( name + "$1" + apcname);
+			Method getAcnames = lib.getDeclaredMethod("getActionNames");
+			String[] names = (String[])getAcnames.invoke(lib.newInstance());
+			for (String n : names) {
+				System.out.println("loading " + n);
+				loader.loadClass(name + "$1" + apcname + "$1" + n);
+			}
 			// get the ActionProvider provider 
 			Method extractAP = lib.getDeclaredMethod("getActionProvider");
 			System.out.println("loaded method");
+			
 			
 			ActionProvider ap = (ActionProvider) extractAP.invoke(lib.newInstance());
 			System.out.println("called extractor");
