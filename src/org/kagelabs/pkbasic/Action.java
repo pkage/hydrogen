1e0a2a9db50e62117f0413f208610819c6049d93
quadnix <quadnix1@gmail.com>
2015-02-16 22:30:46 -0500

    removed old directories

--- src/org/kagelabs/pkbasic/Action.java
+++
@@ -1,7 +0,0 @@
-package org.kagelabs.pkbasic;
-
-public interface Action {
-	public ActionMetadata getMetadata();
-	public void init(ErrorHandler eh);
-	public Value call(ErrorHandler eh, Value[] values);
-}
