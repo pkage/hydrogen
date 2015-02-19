12d26fd28047328c6c182e73bf244d7f7091639d
quadnix <quadnix1@gmail.com>
2015-02-19 10:29:16 -0500

    added script to package externals into jars for loading

---
+++ createjar.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+echo "class name to regex: "
+read fname
+
+mkdir -p org/kagelabs/hydrogen/stl
+cp bin/org/kagelabs/hydrogen/stl/$name* org/kagelabs/hydrogen/stl
+jar cf $fname org
+mv $fname $fname.jar
+rm -r org/
+printf "created jar "
+printf $fname.jar
+printf "\n"
+
