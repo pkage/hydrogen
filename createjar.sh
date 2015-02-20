b12de39b40d421f2f76f0f5b0d063ef8766a192e
quadnix <quadnix1@gmail.com>
2015-02-20 01:24:02 -0500

    fixed a typo causing all compiled classfiles in the stl to be jarred

--- createjar.sh
+++ createjar.sh
@@ -4,7 +4,7 @@ printf "class name to regex: "
 read fname
 
 mkdir -p org/kagelabs/hydrogen/stl
-cp bin/org/kagelabs/hydrogen/stl/$name* org/kagelabs/hydrogen/stl
+cp bin/org/kagelabs/hydrogen/stl/$fname* org/kagelabs/hydrogen/stl
 jar cf $fname org
 mv $fname $fname.jar
 rm -r org/
